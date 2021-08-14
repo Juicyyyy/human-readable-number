77ccd32def1c3d5014b4ea302ad5169ffdbababc
Julia <diniullova2011@yandex.ru>
2021-08-14 17:41:05 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 module.exports = function toReadable (number) {
     const arr = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    const arrDozens = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-
+    const arrDozens = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    
       if (number < 20) {   
-      const word = arr[number];
-      return word;
-    }   
+      const res = arr[number];
+      return res;
+    } 
+     if (number > 20) {
+         str = String(number);
+         n=str[1];
+         const res = arrDozens[0] + arr[n];
+         return res;
+     }
+    
 }
