686ab1b8ff6b718329749d3d0248d6c5a119397e
Julia <diniullova2011@yandex.ru>
2021-08-14 18:29:06 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ module.exports = function toReadable (number) {
         const res = arr[number];
         return res;
     } 
-    else if ((number === 20) || (number === 30) || (number === 40) || (number === 50) || (number === 60) || (number === 70) || (number === 80) || (number === 90))
+    else if ((number === 20) || (number === 30) || (number === 40) || (number === 50) || 
+    (number === 60) || (number === 70) || (number === 80) || (number === 90))
     {
         str = String(number);
         n = str[0];
@@ -21,20 +22,34 @@ module.exports = function toReadable (number) {
         const res = arrDozens[first - 2] + " " + arr[second];
         return res;
     }
-    else if ((number > 100) && (number < 120)){
+    else if ((number > 100) && (number < 120) || ((number > 200) && (number < 220)) || 
+    ((number > 300) && (number < 320)) || ((number > 400) && (number < 420)) || 
+    ((number > 500) && (number < 520)) || ((number > 600) && (number < 620)) || 
+    ((number > 700) && (number < 720)) || ((number > 800) && (number < 820)) || 
+    ((number > 900) && (number < 920))) {
+
         str = String(number);
-        second = str.substring(1, 2);
-        const res = arrHundreds[0] + " " + arr[second];
+        first = str[0];
+        second = str.substring(1, 3);
+        const res = arrHundreds[first - 1] + " " + arr[second];
         return res;
     }
-    else if ((number > 120) && (number < 200)){
+    else if ((number > 120) && (number < 200) || (number > 220) && (number < 300) || 
+    (number > 320) && (number < 400) || (number > 420) && (number < 500) || 
+    (number > 520) && (number < 600) || (number > 620) && (number < 700) || 
+    (number > 720) && (number < 800) || (number > 820) && (number < 900) || 
+    (number > 920) && (number < 1000)) {
+
         str = String(number);
+        first = str[0];
         second = str[1];
         third = str[2]; 
-        const res = arrHundreds[0] + " " + arrDozens[second - 2] + " " + arr[third];
+        const res = arrHundreds[first - 1] + " " + arrDozens[second - 2] + " " + arr[third];
         return res;
     }
-    else if ((number === 200) || (number === 300) || (number === 400) || (number === 500) || (number === 600) || (number === 700) || (number === 800) || (number === 900)){
+    else if ((number === 200) || (number === 300) || (number === 400) || (number === 500) || 
+    (number === 600) || (number === 700) || (number === 800) || (number === 900)){
+
         str = String(number);
         n = str[0];
         const res = arrHundreds[n - 2];
