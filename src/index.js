22bb020e4d65b0faa951a88e19fabff6402c1abf
Julia <diniullova2011@yandex.ru>
2021-08-14 17:12:38 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+    const arr = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const arrDozens = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+
+      if (number < 20) {   
+      const word = arr[number];
+      return word;
+    }   
 }
